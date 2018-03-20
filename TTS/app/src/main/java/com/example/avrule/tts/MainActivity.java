4519efc8c94b459cc23f22b311da13a50002adc1
avrule <abinash7688@gmail.com>
2018-03-20 19:08:07 +0530

    Updated 20-03-2018 working Left issue start permissions

--- TTS/app/src/main/java/com/example/avrule/tts/MainActivity.java
+++ TTS/app/src/main/java/com/example/avrule/tts/MainActivity.java
@@ -7,8 +7,10 @@ import java.io.IOException;
 //import java.net.URI;
 import java.io.StringReader;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 //import java.util.List;
+import java.util.HashMap;
 import java.util.ListIterator;
 import java.util.Locale;
 
@@ -66,6 +68,9 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
     private TextToSpeech tts;
     private Button btnSpeak;
     private EditText txtText;
+    HashMap<String,Integer> map;
+    HashMap<Integer, String> rmap;
+    int [] carr;
     private android.hardware.Camera mCamera;
     private CameraPreview mPreview;
     File pictureFile;
@@ -101,6 +106,7 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
                 @Override
                 public void run() {
                     String ResultString = "";
+                    String TestString = "";
                     final List<ClarifaiOutput<Concept>> predictionResults = client.getDefaultModels().generalModel().predict().
                             withInputs(ClarifaiInput.forImage(new File(pictureFile.getAbsolutePath())))
                             .executeSync()
@@ -131,7 +137,9 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
 
                                     if(cval > 0.9){
                                         try {
-                                            ResultString += " "+concept.name();
+
+                                            ResultString += " "+ProcessText(concept.name());
+                                            TestString += " "+concept.name();
                                             //System.out.println("detected: "+concept.name());
                                         }catch (NullPointerException e){
                                             //System.out.println("No value of Name");
@@ -142,6 +150,9 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
                             }
                         }
                     }
+                    System.out.println("TestString:: "+TestString);
+                    System.out.println("ResultString:: "+ResultString);
+                    Arrays.fill(carr,0);
                     speak(ResultString);
                 }
             });
@@ -156,6 +167,18 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
         }
     };
 
+    private String ProcessText(String string){
+        if(map.containsKey(string)){
+            int val = map.get(string);
+            if(carr[val-1]==0)
+            {
+                carr[val-1]++;
+                return rmap.get(val);
+            }
+        }
+        return "";
+    }
+
 
     /*public void predict(String ImageUrl) {
         final List<ClarifaiOutput<Concept>> predictionResults = client.getDefaultModels().generalModel().predict().
@@ -167,7 +190,7 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
         // Example of a call to a native method
         /*TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());*/
@@ -193,7 +216,13 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
             int requestCode = 100;
             ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.INTERNET}, requestCode);
         }
-
+        setContentView(R.layout.activity_main);
+        map = new HashMap<String, Integer>();
+        addItemstoStiHash();
+        rmap = new HashMap<Integer, String>();
+        addItemstoItsHash();
+        carr = new int[rmap.size()];
+        Arrays.fill(carr,0);
         //client = new ClarifaiBuilder(getString(R.string.CLIENT_ID),getString(R.string.CLIENT_SECRET))
         client = new ClarifaiBuilder(getString(R.string.CLARIFAI_API_KEY))
                 .client(new OkHttpClient())
@@ -201,10 +230,88 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
         Toast.makeText(MainActivity.this,"Before ButtionClick",Toast.LENGTH_SHORT).show();
         //onButtonclicklistener();
         startPreview();
+    }
+
+    private void addItemstoStiHash()
+    {
+        //For person and similar queries 1;
+        map.put("person",1);
+        map.put("people",1);
+        map.put("man",1);
+        map.put("male",1);
+        map.put("female",1);
+        map.put("women",1);
+        map.put("men",1);
+        map.put("woman",1);
+        map.put("boy",1);
+        map.put("girl",1);
+        map.put("child",1);
+        map.put("adult",1);
+
+        map.put("dog",2);
+        map.put("canine",2);
+        map.put("pet",2);
+        map.put("animal",2);
+        map.put("puppy",2);
+        map.put("hound",2);
+        map.put("wolf",2);
+        map.put("fox",2);
+
+        map.put("car",3);
+        map.put("vehicle",3);
+        map.put("transportation system",3);
+        map.put("automotive",3);
+        map.put("drive",3);
+        map.put("sedan",3);
+        map.put("traffic",3);
+        map.put("truck",3);
+        map.put("asphalt",3);
+        map.put("bus",3);
+
+        map.put("road",4);
+        map.put("expressway",4);
+        map.put("lane",4);
+        map.put("highway",4);
+
+        map.put("cattle",5);
+        map.put("mammal",5);
+        map.put("cow",5);
+        map.put("bull",5);
+        map.put("livestock",5);
+        map.put("horn",5);
+        map.put("beef cattle",5);
+        map.put("calf",5);
+
+        map.put("step",6);
+        map.put("handrail",6);
+        map.put("ladder",6);
+        map.put("upstairs",6);
+        map.put("steps",6);
+
+        map.put("engine",7);
+        map.put("locomotive",7);
+        map.put("train",7);
+        map.put("railway",7);
+        map.put("wagon",7);
+
+        map.put("railroad track",8);
+        map.put("track",8);
+
 
     }
 
+    private void addItemstoItsHash()
+    {
+        rmap.put(1,"Human ahead");
+        rmap.put(2,"Dog infront");
+        rmap.put(3,"vehicle ahead");
+        rmap.put(4,"road be careful");
+        rmap.put(5,"Bull infront");
+        rmap.put(6,"Staircase infront careful");
+        rmap.put(7,"Train ahead");
+        rmap.put(8,"Rail track");
 
+    }
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
@@ -299,13 +406,35 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
 
 
     private void startPreview(){
+        System.out.println("before getcamerainstance");
         mCamera = getCameraInstance();
 
         //Create our Preview view and set it as the content of our activity
         mPreview = null;
+        System.out.println("before CameraPreview");
         mPreview = new CameraPreview(this, mCamera);
         FrameLayout preview = findViewById(R.id.camera_preview);
+        System.out.println("before addView");
         preview.addView(mPreview);
+        System.out.println("after addView");
+        tts = new TextToSpeech(this, this);
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                super.run();
+                while (true){
+                    try {
+
+                        mCamera.takePicture(null,null,mPicture);
+                        sleep(5000);
+                    }
+                    catch (Exception e){
+
+                    }
+                }
+            }
+        };
+        thread.start();
     }
 
     private void releaseCamera(){
@@ -341,8 +470,8 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if ((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)){
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_VOLUME_UP)){
             //Do something
             tts = new TextToSpeech(this, this);
             mCamera.takePicture(null,null,mPicture);
@@ -370,6 +499,7 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
             tts.stop();
             tts.shutdown();
         }
+        releaseCamera();
         super.onDestroy();
     }
 
@@ -394,7 +524,7 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
     private void speak(String string){
         //CharSequence text = txtText.getText().toString();
         CharSequence text = (CharSequence)string;
-        tts.setSpeechRate(2);
+        //tts.setSpeechRate(2);
         tts.speak(text ,TextToSpeech.QUEUE_FLUSH,null, null);
     }
 
